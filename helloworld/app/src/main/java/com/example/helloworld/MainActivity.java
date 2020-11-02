f4705be9df76b9b78f5725664ccedc9a461bc09d
yb-ctrlv <yb.ctrlv@gmail.com>
2020-11-02 13:29:23 +0900

    helloworld

---
+++ helloworld/app/src/main/java/com/example/helloworld/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d("MainActivity", "Hello World"); // debug
+        Log.e("MainActivity", "Hello World"); // error
+        Log.w("MainActivity", "Hello World"); // warning
+        Log.i("MainActivity", "Hello World"); // info
+    }
+}
